d51f1b2756640da305e9a1372a5330e622f5f9a9
IanoSang <ianosang18@gmail.com>
2022-05-07 21:55:19 +0300

    feat: models- adds method that will encrpt user password when they sign up

--- app/models.py
+++ app/models.py
@@ -15,3 +15,16 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+
