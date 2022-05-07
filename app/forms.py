d94c6ca711462ac618ea30ae0552c480944b4463
IanoSang <ianosang18@gmail.com>
2022-05-07 22:12:29 +0300

    feat: forms- adds a user sign up funtion

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from app.models import User
+
+
+class RegisterForm(FlaskForm):
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email address already Exists!! Please try a another address')
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=38), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Sign Up')
+
