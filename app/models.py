243fa52be7df3d773332367605a568b1c31e77f2
IanoSang <ianosang18@gmail.com>
2022-05-07 22:01:27 +0300

    feat: user- updates user profile parameters

--- app/models.py
+++ app/models.py
@@ -13,8 +13,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
-    budget = db.Column(db.Integer(), nullable=False, default=1000)
-    items = db.relationship('Item', backref='owned_user', lazy=True)
+    profile = db.Column(db.String(), nullable=False, default='Add you profile Info')
+    posts = db.relationship('Posts', backref='owned_user', lazy=True)
 
     @property
     def password(self):
