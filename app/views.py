20339837d653d161318a9ce1c832515ad940700d
IanoSang <ianosang18@gmail.com>
2022-05-07 22:55:08 +0300

    feat: views- adds a sign up rout and error massages incase of incorrect user inputs

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app.models import User
+from app.forms import RegisterForm
 from app import db
+from flask_login import login_user
 
 
 @app.route('/')
@@ -10,3 +12,16 @@ def home_page():
     return render_template('home.html')
 
 
+@app.route('/signUp', methods=['GET', 'POST'])
+def signUp_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        use_to_create = User(username=form.username.data,
+                             email_address=form.email_address.data, password=form.password1.data)
+        db.session.add(use_to_create)
+        db.session.commit()
+        return redirect(url_for('login_page'))
+    if form.errors != {}:  # If there are no errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+    return render_template('signUp.html', form=form)
