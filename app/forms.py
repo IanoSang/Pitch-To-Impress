fcd42e2345bd5a36c353de7c3e80b0b4802e6777
IanoSang <ianosang18@gmail.com>
2022-05-08 00:44:00 +0300

    feat: imports- adds class pitch to the imports

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
-from app.models import User
+from app.models import User, Pitch
 
 
 def validate_username(username_to_check):
