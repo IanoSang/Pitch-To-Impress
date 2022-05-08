a5c86a03d3d0711a3949672b24cf289ad7a16e83
IanoSang <ianosang18@gmail.com>
2022-05-09 02:28:39 +0300

    feat: static- adds a derectorate that will contains profile photos uploads

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pitches.db'
 app.config['SECRET_KEY'] = 'ed96823c766c274dc3bd19e0'
+app.config['UPLOADED_PHOTOS_DEST'] = 'app/static/photos'
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+photos = UploadSet('photos', IMAGES)
+# configure UploadSet
+configure_uploads(app, photos)
+
+mail = Mail()
+mail.init_app(app)
 
 db = SQLAlchemy(app)
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME='iansangthedev@gmail.com'
+export MAIL_PASSWORD='Iansangthedev1!'
+python3 manage.py server
