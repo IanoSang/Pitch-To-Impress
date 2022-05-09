a2ef5ed0d74abf3588e3b64d8311b19bea243ae6
IanoSang <ianosang18@gmail.com>
2022-05-10 00:44:40 +0300

    feat: change- removes the email configarations

--- config.py
+++ config.py
@@ -2,14 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = 'ed96823c766c274dc3bd19e0'
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'iansangthedev@gmail.com'
-    MAIL_PASSWORD = 'Iansangthedev1!'
+    pass
 
 
 class ProdConfig(Config):
